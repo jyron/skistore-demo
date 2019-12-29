3947ff0f02db8a4f9bb1becdfb47ec97710d50a0
jyron <jyron@unknownf8a2d6da7549.attlocal.net>
2019-12-29 01:44:15 -0800

    product descriptions

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -2,6 +2,7 @@
     <div class="card my-5">
         <div class="card-body">
             <h5 class="text-center card-title">{{product.title}}</h5>
+            <h6 class="text-center">{{product.description}}</h6>
             <p class="text-center text-muted card-text display-4">${{Number(product.price).toFixed()}}</p>
             <button :disabled="isInCart" @click="$emit('add-to-cart',product)" class="btn btn-info form-control">
                 {{isInCart ? 'Added to cart': 'Add to Cart'}}
